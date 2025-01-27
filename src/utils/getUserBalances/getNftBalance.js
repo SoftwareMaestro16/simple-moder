e426e096a633392228dfc8ddee2b520e071a7c3f
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-01-27 17:27:51 +0200

    add

--- src/utils/getUserBalances/getNftBalance.js
+++ src/utils/getUserBalances/getNftBalance.js
@@ -1,15 +1,30 @@
 import axios from "axios";
+import { delay } from "../defay";
 
-async function getNftBalance(walletAddress, collectionAddress) {
+export async function getNftBalance(walletAddress, collectionAddress) {
     const API_URL = `https://tonapi.io/v2/accounts/${walletAddress}/nfts?collection=${collectionAddress}&limit=1000&offset=0&indirect_ownership=false`;
 
-    return axios
-        .get(API_URL)
-        .then(response => response.data.nft_items || [])
-        .catch(error => {
-            console.error("Error fetching NFTs:", error);
-            return [];
-        });
-}
+    try {
+        const response = await axios.get(API_URL);
 
-export default getNftBalance;
+        if (response.data && response.data.nft_items) {
+            return response.data.nft_items;
+        }
+
+        return [];
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 429) {
+                console.warn('Rate limit exceeded. Retrying...');
+                await delay(5000);
+                return getNftBalance(walletAddress, collectionAddress); 
+            }
+
+            if (error.response.status === 404) {
+                console.log('NFT collection or wallet not found (404). Returning 0.');
+                return 0; 
+            }
+        }
+        throw error;
+    }
+}
