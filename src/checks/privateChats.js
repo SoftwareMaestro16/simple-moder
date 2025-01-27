ddc4b2fc83e4a0aefba75de2feb75a6633c8cce4
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-01-27 20:16:20 +0200

    add

--- src/checks/privateChats.js
+++ src/checks/privateChats.js
@@ -4,52 +4,54 @@ import { nftPrivateChat } from "./privateNftChat.js";
 import { comboPrivateChat } from "./privateComboChat.js"; 
 
 export async function handlePrivateChats(bot) {
-    try {
-        const updateChats = async () => {
-            const jettonChats = await getAllPrivateJettonChats();
-            const nftChats = await getAllPrivateNftChats();
-            const comboChats = await getAllComboChats();
+  try {
+    let jettonChats = [];
+    let nftChats = [];
+    let comboChats = [];
 
-            // console.log("Jetton Chats:", jettonChats);
-            // console.log("NFT Chats:", nftChats);
-            // console.log("Combo Chats:", comboChats);
+    const updateChats = async () => {
+      jettonChats = await getAllPrivateJettonChats();
+      nftChats = await getAllPrivateNftChats();
+      comboChats = await getAllComboChats();
 
-            bot.on("chat_join_request", async (msg) => {
-                try {
-                    const chatId = String(msg.chat.id);
+      // console.log("Jetton Chats:", jettonChats);
+      // console.log("NFT Chats:", nftChats);
+      // console.log("Combo Chats:", comboChats);
+    };
 
-                    const isComboChat = comboChats.some((chat) => String(chat.chatId) === chatId);
-                    if (isComboChat) {
-                        console.log(`Запрос на вступление в Combo-чат: ${chatId}`);
-                        await comboPrivateChat({ chatId, msg, bot });
-                    } else {
-                        const isNftChat = nftChats.some((chat) => String(chat.chatId) === chatId);
+    bot.on("chat_join_request", async (msg) => {
+      try {
+        const chatId = String(msg.chat.id);
 
-                        if (isNftChat) {
-                            console.log(`Запрос на вступление в NFT-чат: ${chatId}`);
-                            await nftPrivateChat({ chatId, msg, bot });
-                        } else {
-                            const isJettonChat = jettonChats.some((chat) => String(chat.chatId) === chatId);
-
-                            if (isJettonChat) {
-                                console.log(`Запрос на вступление в Jetton-чат: ${chatId}`);
-                                await jettonPrivateChat({ chatId, msg, bot });
-                            } else {
-                                console.log(`Чат ${chatId} не найден в списке Jetton- или NFT-чатов.`);
-                            }
-                        }
-                    }
-                } catch (error) {
-                    console.error('Ошибка при обработке запроса на вступление в чат:', error.message);
-                }
-            });
+        const isComboChat = comboChats.some((chat) => String(chat.chatId) === chatId);
+        if (isComboChat) {
+          console.log(`Запрос на вступление в Combo-чат: ${chatId}`);
+          await comboPrivateChat({ chatId, msg, bot });
+        } else {
+          const isNftChat = nftChats.some((chat) => String(chat.chatId) === chatId);
+          if (isNftChat) {
+            console.log(`Запрос на вступление в NFT-чат: ${chatId}`);
+            await nftPrivateChat({ chatId, msg, bot });
+          } else {
+            const isJettonChat = jettonChats.some((chat) => String(chat.chatId) === chatId);
+            if (isJettonChat) {
+              console.log(`Запрос на вступление в Jetton-чат: ${chatId}`);
+              await jettonPrivateChat({ chatId, msg, bot });
+            } else {
+              console.log(`Чат ${chatId} не найден в списке Jetton-, NFT- или Combo-чатов.`);
+            }
+          }
         }
+      } catch (error) {
+        console.error('Ошибка при обработке запроса на вступление в чат:', error.message);
+      }
+    });
 
-        setInterval(updateChats, 4000); 
+    await updateChats();
 
-        await updateChats();
+    setInterval(updateChats, 4000);
 
-    } catch (error) {
-        console.error('Ошибка при настройке обработчика приватных чатов:', error.message);
-    }
+  } catch (error) {
+    console.error('Ошибка при настройке обработчика приватных чатов:', error.message);
+  }
 }
